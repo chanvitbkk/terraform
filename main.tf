406fe6e93860fd8d5b2c9384085f6df5ad599696
chanvitbkk <chanvit.moungyoo@icloud.com>
2024-05-20 16:50:48 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = ""
-  secret_key = ""
+  access_key = "AKIA47CR2PIOAETCHCJ5"
+  secret_key = "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"
 }
 # Create a VPC
 resource "aws_vpc" "TeeVPC" {
